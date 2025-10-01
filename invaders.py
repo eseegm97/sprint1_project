92ce05aa33ea12413f3c1e5fa63037447d2be5e8
eseegm97 <eseegm97@gmail.com>
2025-10-01 07:14:06 -0500

    invaders

---
+++ invaders.py
@@ -0,0 +1,78 @@
+import turtle
+
+CANNON_STEP = 10
+
+#Create the game screen/window
+window = turtle.Screen()
+window.tracer(0)
+window.setup(0.5, 0.75)
+window.bgcolor(0.2, 0.2, 0.2)
+window.title("Ethan's Alien Invaders")
+
+LEFT = -window.window_width() / 2
+RIGHT = window.window_width() / 2
+TOP = window.window_height() / 2
+BOTTOM = -window.window_height() / 2
+FLOOR_LEVEL = 0.9 * BOTTOM
+GUTTER = 0.025 * window.window_width()
+
+#Create the laser cannon
+cannon = turtle.Turtle()
+cannon.penup()
+cannon.color(1, 1, 1)
+cannon.shape("square")
+cannon.setposition(0, FLOOR_LEVEL)
+
+lasers = []
+
+#Cannon shape and size
+def draw_cannon():
+    cannon.clear()
+    cannon.turtlesize(1, 4)
+    cannon.stamp()
+    cannon.sety(FLOOR_LEVEL + 10)
+    cannon.turtlesize(1, 1.5)
+    cannon.stamp()
+    cannon.sety(FLOOR_LEVEL + 20)
+    cannon.turtlesize(0.8, 0.3)
+    cannon.stamp()
+    cannon.sety(FLOOR_LEVEL)
+    window.update()
+
+#Cannon movement
+def move_left():
+    new_x = cannon.xcor() - CANNON_STEP
+    if new_x >= LEFT + GUTTER:
+        cannon.setx(new_x)
+        draw_cannon()
+
+def move_right():
+    new_x = cannon.xcor() + CANNON_STEP
+    if new_x <= RIGHT - GUTTER:
+        cannon.setx(new_x)
+        draw_cannon()
+
+#Create the lasers
+def create_laser():
+    laser = turtle.Turtle()
+    laser.penup()
+    laser.color(1, 0, 0)
+    laser.hideturtle()
+    laser.setposition(cannon.xcor(), cannon.ycor())
+    laser.setheading(90)
+    laser.forward(20)
+    laser.pendown()
+    laser.pensize(5)
+
+    lasers.append(laser)
+
+#Key bindings
+window.onkeypress(move_left, "Left")
+window.onkeypress(move_right, "Right")
+window.onkeypress(create_laser, "space")
+window.onkeypress(turtle.bye, "q")
+window.listen()
+
+draw_cannon()
+
+turtle.done()
